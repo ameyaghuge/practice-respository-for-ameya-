171141feaf24a04ddbef93e3729378fc1d5fb43f
ameyaghuge <ameyaghuge@gmail.com>
2024-11-09 08:55:17 +1100

    Add files via upload
    My first ever github repo!

---
+++ ventolin.py
@@ -0,0 +1,23 @@
+# I want to create a program where i ask where the ventolin puffer is and it tells me that it is right next to me 
+
+#we need an input first 
+# then we need to define it 
+# and then return it to the user as well 
+where = input
+
+def ventolin ():
+    global where
+    where = input("Is your ventonlin puffer here?")
+    if where == "yes":
+        print("Yes, it's right next to me.")
+        print("CONGRATULATIONS! YOU FOUND IT AWESOME, p.s you should take it now the pollen count outside it horrilbe")
+    else: where == "no"
+    print( "look around you it should be inside the house somewhere.")
+    print("don't worry you will find it :)")
+    
+ 
+ventolin()
+
+#this is my first ever program i wrote by myself for practice I'm asthmatic so this is helpful for me, 
+# anways it was good practice and it was fun :)
+# till next time github :))
