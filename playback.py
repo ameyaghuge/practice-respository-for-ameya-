b8908144c530efcb7bc14d0152b47e2e861d0c92
ameyaghuge <ameyaghuge@gmail.com>
2024-11-17 17:10:14 +1100

    Add files via upload
    Problem set 0 
    this is the playback.py code i finally figured it out, it was good practice

---
+++ playback.py
@@ -0,0 +1,14 @@
+#implement a program in Python that prompts the user for input and then outputs that same input, replacing each space with ... (i.e., three periods).
+
+#So we need an input 
+#We need an output
+#We need to replace each output with ... to replace each space
+
+#input here 
+Speak = input("What is it you wish to speak?")
+#here it should replace the spaces with ... 
+#how do i replace each space with ...?
+publish = Speak.replace(" ", "…")
+
+#output here
+print(publish)
